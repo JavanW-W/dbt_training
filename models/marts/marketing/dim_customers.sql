5907d655c6377bf0d03d08f157c45c40ad208442
Javan Whitney-Warner <javan.whitney-warner@numat.com>
2025-07-14 16:42:07 +0000

    Update customers models to go with video tutorial

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,19 +1,15 @@
-{{
-  config(
-    materialized='view'
-  )
-}}
-
 with customers as (
 
-    select * from {{ ref('stg_customers') }}
+select *
+from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
-
+    select *
+    from {{ ref('stg_orders') }}
+    
 ),
 
 customer_orders as (
